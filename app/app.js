54454493f43ae31df0a04b5e77e6396aa40be745
Ryan Wire <ryanwire@outlook.com>
2020-11-30 17:23:30 +0300

    Sets up ng-show and ng-hide directives

--- app/app.js
+++ app/app.js
@@ -5,22 +5,26 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
     {
       name: "Yoshi",
       belt: "green",
-      rate: 50
+      rate: 50,
+      available: true
     },
     {
       name: "Crystal",
       belt: "yellow",
-      rate: 30
+      rate: 30,
+      available: true
     },
     {
       name: "Ryu",
       belt: "orange",
-      rate: 10
+      rate: 10,
+      available: true
     },
     {
       name: "Ryan",
       belt: "black",
-      rate: 1000
+      rate: 1000,
+      available: false
     }
   ]
 }])
