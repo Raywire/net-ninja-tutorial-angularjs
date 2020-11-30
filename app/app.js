baca93a970b1e9f55fb1b38c1822f40470db36fd
Ryan Wire <ryanwire@outlook.com>
2020-11-30 20:40:32 +0300

    Sets up ng-submit directive

--- app/app.js
+++ app/app.js
@@ -5,6 +5,18 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
     const removedNinja = $scope.ninjas.indexOf(ninja)
     $scope.ninjas.splice(removedNinja, 1)
   }
+  $scope.addNinja = function() {
+    $scope.ninjas.push({ 
+      name: $scope.newNinja.name,
+      belt: $scope.newNinja.belt,
+      rate: parseInt($scope.newNinja.rate),
+      available: true
+    })
+
+    $scope.newNinja.name = ''
+    $scope.newNinja.belt = ''
+    $scope.newNinja.rate = ''
+  }
   $scope.ninjas = [
     {
       name: "Yoshi",
