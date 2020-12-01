ee05163d3f2d5818f4fabc3b079c8cfa9aff89d3
Ryan Wire <ryanwire@outlook.com>
2020-12-01 17:24:06 +0300

    Comments out logic for pretty urls

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const myNinjaApp = angular.module('myNinjaApp', ['ngRoute', 'ngAnimate'])
 
 myNinjaApp.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {
 
-  $locationProvider.html5Mode(true)
+  // $locationProvider.html5Mode(true)
 
   $routeProvider
     .when('/home', {
